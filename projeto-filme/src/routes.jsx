e5691c04e12c00f9239440d22c859149060b9c93
erliwanderson <wandersonerli.dev@gmail.com>
2023-10-11 00:59:59 -0300

    :bug: fix: Atualização dá lógica

--- projeto-filme/src/routes.jsx
+++ projeto-filme/src/routes.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
 import Erro from "./pages/Error";
+import Favoritos from "./pages/Favoritos";
 
 import Layout from "./components/Layout";
 
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
       {
         path: "/filme/:id",
         element: <Filme />,
+      },
+      {
+        path: "/favoritos",
+        element: <Favoritos />,
       },
       {
         path: "*",
